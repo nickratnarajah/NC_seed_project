36d4a33edfad787d3eabb6a6775051191e933210
Nick <nj.ratnarajah@gmail.com>
2025-05-01 09:33:33 +0100

    add 4-get-articles

--- source/models/news.model.js
+++ source/models/news.model.js
@@ -15,7 +15,14 @@ return db.query('SELECT * FROM topics')
 }
 
 const selectArticles = () => {
-    return db.query(`SELECT * FROM articles`)
+    return db.query(`SELECT articles.author, articles.title, articles.article_id, articles.topic, articles.created_at, articles.votes, articles.article_img_url,
+        COUNT (comments.comment_id) ::int AS comment_count
+        FROM articles
+        LEFT JOIN comments
+        ON articles.article_id = comments.article_id
+        GROUP BY 
+        articles.article_id
+        ORDER BY articles.created_at DESC`)
     .then((result) => {
         const articles = result.rows
         return articles
