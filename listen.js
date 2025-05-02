55cc01d647ad3c9591ac95d17f91d38cd0561469
Nick <nj.ratnarajah@gmail.com>
2025-05-02 11:10:40 +0100

    change api to app in listen file

--- listen.js
+++ listen.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-const api = require("./api")
+const app = require("./api")
 const { PORT = 9090 } = process.env
 
 app.listen(PORT, () => console.log(`Server is listening on ${PORT}...`));
