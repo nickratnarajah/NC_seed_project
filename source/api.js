b0daa25e2ea39192a95a2cd93743b5240022131d
Nick <nj.ratnarajah@gmail.com>
2025-05-19 09:34:38 +0100

    allow cross origin resource sharing

--- source/api.js
+++ source/api.js
@@ -3,9 +3,11 @@ require('../db/connection');
 const express = require('express');
 const apiRouter = require ("./routers/api-router")
 const { handleCustomErrors, handlePsqlErrors, handleServerErrors, handlePathNotFound } = require('./errors/news.errors');
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use("/api", apiRouter);
 
